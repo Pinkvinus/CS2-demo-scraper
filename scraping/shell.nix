ddf67458703156f11cd1e651f7855aa3bc799979
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2024-11-10 20:46:08 +0100

    added shell for cool nix users

---
+++ scraping/shell.nix
@@ -0,0 +1,13 @@
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    pkgs.wget
+    (pkgs.python3.withPackages (python-pkgs: [
+      # select Python packages here
+      python-pkgs.cloudscraper
+    ]))
+  ];
+}
